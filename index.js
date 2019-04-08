ad0f0414dbe48c7de7d68bfa1b800e1f2937ed0e
Crhistian Raphael <crhistian_raphael@hotmail.com>
2019-04-08 07:09:24 -0300

    feat(xxx): can't figure how to get commits without publishing a PR first

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const github = require('octonode');
 const fs = require('fs');
 
 var client = github.client(process.env.GITHUB_TOKEN);
-var ghrepo = client.repo('crhraphael/gitbasan');
 var cwd = process.cwd();
 
 var prBody = {
@@ -29,6 +28,7 @@ const branchName = headFile.toString().split(':')[1].split('\\')[0].trim()
 let configFile = fs.readFileSync(configFilePath).toString();
 const repoName = configFile.match(/url\ =\ (.*)\n/gm)[0].trim().split(':')[1]
 console.log(repoName);
+var ghrepo = client.repo(repoName);
 
     var prBody = {
         "title": "Adding branch name recognition",
